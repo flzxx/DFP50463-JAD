5a6a3580a6beb3cb890c9a9683cadfcb6c349fb7
mary <maryallendrea@gmail.com>
2026-01-19 08:59:17 +0800

    Layout

--- W2/spmp/src/my/edu/spmp/app/HelpScreen.java
+++ W2/spmp/src/my/edu/spmp/app/HelpScreen.java
@@ -1,12 +1,20 @@
 package my.edu.spmp.app;
 
-import javax.swing.JFrame;
+import java.awt.BorderLayout;
+import java.awt.Color;
+
+import javax.swing.*;
 
 public class HelpScreen {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		JFrame frame = new JFrame();
+		JPanel bluePanel = new JPanel();
+		bluePanel.setBackground(Color.blue);
+		bluePanel.setBounds(5, 0, 100, 100);
+		frame.setLayout(new BorderLayout());
+		frame.add(bluePanel, BorderLayout.CENTER);
 		frame.setSize(400, 400);
 		frame.setVisible(true);
 
